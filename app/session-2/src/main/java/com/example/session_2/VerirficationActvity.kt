f92de54b6cb5bd258dbefa5ac935ea900e3ea17f
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-06-28 16:51:53 +0300

    session2Complete

---
+++ app/session-2/src/main/java/com/example/session_2/VerirficationActvity.kt
@@ -0,0 +1,11 @@
+package com.example.session_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerirficationActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verirfication_actvity)
+    }
+}
