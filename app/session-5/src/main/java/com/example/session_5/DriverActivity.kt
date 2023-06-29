4a4d612e9c4de1c0371a5319c442578013d1e8af
MrKot007 <arseniy.bobovnikov@gmail.com>
2023-06-29 14:05:08 +0300

    session5complete

---
+++ app/session-5/src/main/java/com/example/session_5/DriverActivity.kt
@@ -0,0 +1,11 @@
+package com.example.session_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DriverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver)
+    }
+}
